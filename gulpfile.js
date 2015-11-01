8caf01d157c8ca4e07fcee4ee0788281d541c212
Blake Regalia <blake.regalia@gmail.com>
2015-11-01 11:23:43 -0800

    travis

--- gulpfile.js
+++ gulpfile.js
@@ -71,5 +71,5 @@ gulp.task('clean', function () {
   return del('dist');
 });
 
-gulp.task('prepublish', ['nsp', 'babel']);
+gulp.task('prepublish', ['babel']);
 gulp.task('default', ['static', 'test', 'coveralls']);
