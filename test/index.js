1d567570bc03d86b1f910248bffb866ef841427d
Blake Regalia <blake.regalia@gmail.com>
2015-11-03 12:58:23 -0800

    graphy

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
-import ldquery from '../lib';
+import graphy from '../lib';
 
-describe('ldquery', function () {
+describe('graphy', function () {
   it('should have unit test!', function () {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
