a67c114fd278ea762389172e13c4cd9f3659f0bd
Blake Regalia <blake.regalia@gmail.com>
2020-04-17 23:48:20 -0700

    fix(n-scan): scribe preset buffer rinsing

--- test/package/content.nt.scan.js
+++ test/package/content.nt.scan.js
@@ -7,8 +7,8 @@ const once = events.once;
 const factory = require('@graphy/core.data.factory');
 const dataset_tree = require('@graphy/memory.dataset.fast');
 
+const ttl_reader = require('@graphy/content.ttl.read');
 const nt_scriber = require('@graphy/content.nt.scribe');
-
 const nt_scan = require('@graphy/content.nt.scan');
 
 function emulate_stream(ab_input) {
@@ -31,6 +31,8 @@ function emulate_stream(ab_input) {
 	});
 }
 
+const P_NS_EG = 'http://example.org/linked-open-data/instance-or-ontology/';
+
 describe('content.nt.scan', () => {
 	const NL_GENERATE = 0x10000 * 64;  // approximately 4 MiB
 	let ab_generate;
@@ -46,16 +48,14 @@ describe('content.nt.scan', () => {
 			s_generate += s_write;
 		});
 
-		let p_ns = 'http://example.org/linked-open-data/instance-or-ontology/';
-
 		for(; s_generate.length<NL_GENERATE; c_items++) {
 			ds_scriber.write(factory.quad(...[
-				factory.namedNode(p_ns+'subject-'+c_items),
-				factory.namedNode(p_ns+'predicate-'+c_items),
+				factory.namedNode(P_NS_EG+'subject-'+c_items),
+				factory.namedNode(P_NS_EG+'predicate-'+c_items),
 				(c_items % 5)
-					? factory.namedNode(p_ns+'object-'+c_items)
+					? factory.namedNode(P_NS_EG+'object-'+c_items)
 					: (c_items % 2)
-						? factory.literal('some -value', factory.namedNode(p_ns+'datatype-'+c_items))
+						? factory.literal('some -value', factory.namedNode(P_NS_EG+'datatype-'+c_items))
 						: factory.literal('This string is an example literal data for the generated dataset, item number "'+c_items+'".', 'en'),
 			]));
 		}
@@ -80,121 +80,136 @@ describe('content.nt.scan', () => {
 			// force set workers to zero
 			ds_scanner._nl_workers = 0;
 
-			emulate_stream(ab_generate)
-				.pipe(ds_scanner);
+			ds_scanner.import(emulate_stream(ab_generate));
 		});
 	});
 
 	describe('basics', () => {
 		it('blank', (fke_test) => {
-			emulate_stream(Buffer.allocUnsafe(0))
-				.pipe(nt_scan({
-					preset: 'count',
-					report(c_quads) {
-						expect(c_quads).to.equal(0);
-						fke_test();
-					},
-				}));
+			nt_scan({
+				preset: 'count',
+				report(c_quads) {
+					expect(c_quads).to.equal(0);
+					fke_test();
+				},
+			}).import(emulate_stream(Buffer.allocUnsafe(0)));
 		});
 
 		it('one line', (fke_test) => {
-			emulate_stream(Buffer.from(`<z://a/> <z://b/> <z://c/> .`))
-				.pipe(nt_scan({
-					preset: 'count',
-					report(c_quads) {
-						expect(c_quads).to.equal(1);
-						fke_test();
-					},
-				}));
+			nt_scan({
+				preset: 'count',
+				report(c_quads) {
+					expect(c_quads).to.equal(1);
+					fke_test();
+				},
+			}).import(emulate_stream(Buffer.from(`<z://a/> <z://b/> <z://c/> .`)));
 		});
 
 		it('two lines', (fke_test) => {
-			emulate_stream(Buffer.from(`<z://a/> <z://b/> <z://c/> .\n<z://a/> <z://b/> "c"@en .`))
-				.pipe(nt_scan({
-					preset: 'count',
-					report(c_quads) {
-						expect(c_quads).to.equal(2);
-						fke_test();
-					},
-				}));
+			nt_scan({
+				preset: 'count',
+				report(c_quads) {
+					expect(c_quads).to.equal(2);
+					fke_test();
+				},
+			}).import(emulate_stream(Buffer.from(`<z://a/> <z://b/> <z://c/> .\n<z://a/> <z://b/> "c"@en .`)));
 		});
 	});
 
 	describe('errors', () => {
 		it('invalid master', (fke_test) => {
-			emulate_stream(Buffer.from('Invalid.'))
-				.pipe(nt_scan({
-					preset: 'count',
+			nt_scan({
+				preset: 'count',
 
-					error(e_read) {
-						fke_test();
-					},
+				error(e_read) {
+					fke_test();
+				},
 
-					report() {
-						fke_test(new Error('failed to stop report after invalid input'));
-					},
-				}));
+				report() {
+					fke_test(new Error('failed to stop report after invalid input'));
+				},
+			}).import(emulate_stream(Buffer.from('Invalid.')));
 		});
 
 		it('invalid worker', (fke_test) => {
-			emulate_stream(Buffer.from('<z://a/> <z://b/> <z://c/> .\nInvalid.'))
-				.pipe(nt_scan({
-					preset: 'count',
+			nt_scan({
+				preset: 'count',
 
-					error(e_read) {
-						fke_test();
-					},
+				error(e_read) {
+					fke_test();
+				},
 
-					report() {
-						fke_test(new Error('failed to stop report after invalid input'));
-					},
-				}));
+				report() {
+					fke_test(new Error('failed to stop report after invalid input'));
+				},
+			}).import(emulate_stream(Buffer.from('<z://a/> <z://b/> <z://c/> .\nInvalid.')));
 		});
 	});
 
 	describe('tasks', () => {
 		it('preset: count', (fke_test) => {
-			emulate_stream(ab_generate)
-				.pipe(nt_scan({
-					preset: 'count',
+			nt_scan({
+				preset: 'count',
 
-					report(c_quads) {
-						expect(c_quads).to.equal(c_items);
-						fke_test();
-					},
-				}));
+				report(c_quads) {
+					expect(c_quads).to.equal(c_items);
+					fke_test();
+				},
+			}).import(emulate_stream(ab_generate));
 		});
 
 		it('preset: scribe', (fke_test) => {
-			emulate_stream(ab_generate)
-				.pipe(nt_scan({
-					preset: 'scribe',
+			let c_quads = 0;
 
-					user: {
-						prefixes: {
-							'': 'http://example.org/linked-open-data/instance-or-ontology/',
-						},
-					},
+			let ds_verifier = ttl_reader({
+				error(e_read) {
+					fke_test(new Error(`scriber output was invalid: ${e_read.stack}`));
+				},
 
-					update() {
-						expect(c_quads).to.equal(c_items);
-					},
+				data(g_quad) {
+					c_quads += 1;
+				},
 
-					end() {
-						fke_test();
+				eof() {
+					expect(c_quads).to.equal(c_items);
+					fke_test();
+				},
+			});
+
+			nt_scan({
+				preset: 'scribe',
+
+				spawn: () => ({
+					prefixes: {
+						'': P_NS_EG,
 					},
-				}));
+				}),
+
+				// transform chunks received from worker back into strings
+				receive: {
+					update: ab_chunk => Buffer.from(ab_chunk).toString('utf8'),
+				},
+
+				// write to verifier
+				update(s_scribe) {
+					ds_verifier.write(s_scribe);
+				},
+
+				// once scan is done
+				report() {
+					ds_verifier.end();
+				},
+			}).import(emulate_stream(ab_generate));
 		});
 
-		it('preset: distinct-quads', () => {
-			emulate_stream(ab_generate)
-				.pipe(nt_scan({
-					preset: 'distinct-quads',
+		// it('preset: distinct-quads', () => {
+		// 	nt_scan({
+		// 		preset: 'distinct-quads',
 
-					report(c_quads) {
-						expect(c_quads).to.equal(c_items);
-					},
-				}));
-		});
+		// 		report(c_quads) {
+		// 			expect(c_quads).to.equal(c_items);
+		// 		},
+		// 	}).import(emulate_stream(ab_generate));
+		// });
 	});
 });
