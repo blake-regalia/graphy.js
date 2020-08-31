d04a702116b7e30c28e71e585284eaca7810a21d
Blake Regalia <blake.regalia@gmail.com>
2020-08-30 23:36:13 -0700

    feat: basic exports for content

---
+++ src/content/main.mjs
@@ -0,0 +1,42 @@
+import NTriplesReader from '@graphy/content.nt.read';
+import NTriplesScanner from '@graphy/content.nt.scan';
+import NTriplesScriber from '@graphy/content.nt.scribe';
+import NTriplesWriter from '@graphy/content.nt.write';
+
+import NQuadsReader from '@graphy/content.nq.read';
+import NQuadsScanner from '@graphy/content.nq.scan';
+import NQuadsScriber from '@graphy/content.nq.scribe';
+import NQuadsWriter from '@graphy/content.nq.write';
+
+import TurtleReader from '@graphy/content.ttl.read';
+import TurtleScriber from '@graphy/content.ttl.scribe';
+import TurtleWriter from '@graphy/content.ttl.write';
+
+import TrigReader from '@graphy/content.trig.read';
+import TrigScriber from '@graphy/content.trig.scribe';
+import TrigWriter from '@graphy/content.trig.write';
+
+import XmlScriber from '@graphy/content.xml.scribe';
+
+
+export {
+	NTriplesReader,
+	NTriplesScanner,
+	NTriplesScriber,
+	NTriplesWriter,
+
+	NQuadsReader,
+	NQuadsScanner,
+	NQuadsScriber,
+	NQuadsWriter,
+
+	TurtleReader,
+	TurtleScriber,
+	TurtleWriter,
+
+	TrigReader,
+	TrigScriber,
+	TrigWriter,
+
+	XmlScriber,
+};
